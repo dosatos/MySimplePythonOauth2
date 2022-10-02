a0054a13d3364a9f91f2f49484ca8452e480fa4d
Yeldos Balgabekov <byeldos@amazon.com>
2022-10-02 17:09:09 +0200

    Improved auth token logic and added Makefile

--- test/token/test_token.py
+++ test/token/test_token.py
@@ -2,30 +2,37 @@ import pytest
 
 from auth_token.token import TokenEncoder, TokenDecoder
 
-with open('../jwt-key') as fh:
+with open('./jwt-key', encoding='utf-8') as fh:
     PRIVATE_KEY = fh.read()
 
-with open('../jwt-key.pub') as fh:
+with open('./jwt-key.pub', encoding='utf-8') as fh:
     PUBLIC_KEY = fh.read()
 
 SECRET_SYMMETRIC_KEY = 'SECRET_SYMMETRIC_KEY'
 
 
+SECRETS_CONFIG = {
+    'private': PRIVATE_KEY,
+    'public': PUBLIC_KEY,
+    'symmetric': SECRET_SYMMETRIC_KEY,
+}
+
+
 @pytest.mark.parametrize(
-    ("algorithm", "encrypt_key", "decrypt_key"),
+    ('algorithm', 'encrypt_key', 'decrypt_key'),
     [
-        ("HS256", SECRET_SYMMETRIC_KEY, SECRET_SYMMETRIC_KEY),
-        ("RS256", PRIVATE_KEY, PUBLIC_KEY),
-    ]
+        ('HS256', 'symmetric', 'symmetric'),
+        ('RS256', 'private', 'public'),
+    ],
 )
 def test_encode_decode_rsa256(algorithm, encrypt_key, decrypt_key):
-    payload = {"key": "value"}
+    payload = {'key': 'value'}
     encoder = TokenEncoder(
         algorithm,
-        encrypt_key=encrypt_key,
-        decrypt_key=decrypt_key
+        encrypt_key=SECRETS_CONFIG[encrypt_key],
+        decrypt_key=SECRETS_CONFIG[decrypt_key],
     )
-    decoder = TokenDecoder(algorithm, decrypt_key)
+    decoder = TokenDecoder(algorithm, SECRETS_CONFIG[decrypt_key])
 
     token = encoder.encode(payload)
     assert token.payload == decoder.decode(token.value)
